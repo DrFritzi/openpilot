3d5e093b8b61e73e09a2457441386a0e83bf8a6b
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-03-12 11:52:35 -0500

    Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -24,7 +24,7 @@ class CarInterface(CarInterfaceBase):
     ret.steerRatio = 16.2  # Pacifica Hybrid 2017
     ret.mass = 2858. + STD_CARGO_KG  # kg curb weight Pacifica Hybrid 2017
     ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[0., 20.], [0., 20.]]
-    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.30], [0.05, 0.08]]
+    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.05, 0.150], [0.05, 0.08]]
     ret.lateralTuning.pid.kf = 0.00006   # full torque for 10 deg at 80mph means 0.00007818594
     ret.steerActuatorDelay = 0.1
     ret.steerRateCost = 0.5
